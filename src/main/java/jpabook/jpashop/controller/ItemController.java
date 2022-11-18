302adbe4d78afcc11767201f468b26ca87747251
mingsound <62479120+mingsound21@users.noreply.github.com>
2022-11-19 02:06:28 +0900

    ch 06. 상품 등록

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+        // 리팩토링: setter 대신 createBook 정적 팩토리 메서드 만드는 게 좋음. + setter 삭제
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+}
