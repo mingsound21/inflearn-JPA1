347fcd80e84d9e5ea8ff0fa1e44cebe6bfe94181
mingsound <62479120+mingsound21@users.noreply.github.com>
2022-11-19 01:52:47 +0900

    ch 06. 회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,6 +39,16 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
-
     }
+
+    @GetMapping("/members")
+    public String list(Model model){ // Model이라는 객체 통해서 화면에 객체 전달
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+    // Q. 회원 가입일 땐 폼 객체를 사용해서 정보를 받았는데, 회원 목록 조회는 그냥 Member 엔티티 사용?
+    // A. 단순해서 Member 엔티티 자체를 사용했을 뿐, 실무에서는 DTO로 변환해서 화면에 꼭 필요한 것만 가진 객체를 만드는 것이 좋다!
+    // 단, api를 만들때에는 이유불문 엔티티 외부로 넘겨서는 안됨
+    // => 엔티티 변경 시, api 스펙(response body)가 변함 + 개인정보 노출 가능성
 }
